120e44278cf808f468961074c5ce5bfb3443c1fa
VHSDO <santosdeoliveiravictorhugo1@gmail.com>
2025-07-23 13:57:06 -0300

    Add files via upload

--- Projeto final/Projeto/src/app/services/login.service.ts
+++ Projeto final/Projeto/src/app/services/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Usuario } from '../../../../../Sprint Angular/Sprint Angular/Angular/aula-angular-ford-main/src/app/models/user';
+import { Usuario } from '../models/user'
 import { Observable, tap } from 'rxjs';
 
 @Injectable({
